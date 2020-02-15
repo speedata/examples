9be99cb76b69f750a29b1297772c53a7c77338cd
Patrick Gundlach <gundlach@speedata.de>
2020-02-15 10:00:15 +0100

    Parameters for the json2xml transformation
    Needs speedata Publisher >= 3.9.5

--- technical/jsonreader/json2xml.lua
+++ technical/jsonreader/json2xml.lua
@@ -1,9 +1,13 @@
 -- requires speedata Publisher version >= 3.9.4
 
 runtime = require("runtime")
-ok, msg = runtime.run_saxon({stylesheet = 'json2xml.xsl', out = 'data.xml',initialtemplate='main'})
+ok, msg = runtime.run_saxon({
+    stylesheet = 'json2xml.xsl',
+    out = 'data.xml',
+    initialtemplate='main',
+    params = 'filename=us-500.json'})
 
--- den Publishing-Prozess abbrechen, wenn die Transformation fehl schlägt.
+-- stop the publishing run when anything got wrong
 if not ok then
     print(msg)
     os.exit(-1)
--- technical/jsonreader/json2xml.xsl
+++ technical/jsonreader/json2xml.xsl
@@ -1,8 +1,10 @@
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="3.0" default-mode="main">
   <xsl:output indent="yes"/>
 
+  <xsl:param name="filename" select="'us-500.json'"/>
+
   <xsl:template name="main">
-    <xsl:variable name="doc" select="json-to-xml(unparsed-text('us-500.json'))"/>
+    <xsl:variable name="doc" select="json-to-xml(unparsed-text($filename))"/>
     <xsl:copy-of select="$doc"/>
   </xsl:template>
 </xsl:stylesheet>
